70aa232eb020a037fee56ccfbe40bca7d44e36ff
Mohamed Sayed <ms10596@gmail.com>
2018-08-23 16:01:20 +0200

    feat: making html files for adding, deleting and editing. mapping them with links

--- app.py
+++ app.py
@@ -2,63 +2,77 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Item
+
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///catalog')
 Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind = engine)
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
 
 @app.route('/')
 def homepage():
     categories = session.query(Item.category).distinct()
-
-
     movies = session.query(Item).order_by(Item.id.desc()).all()
-
     return render_template('menu.html', categories=categories, movies=movies)
+
 
 @app.route('/catalog/<string:categoryName>/items')
 def categoryPage(categoryName):
-    items = session.query(Item.name).filter_by(category = categoryName)
-    output = ''
-    for item in items:
-        output += str(item)[3:-3]
-        output += '</br>'
-    return output
+    items = session.query(Item.name).filter_by(category=categoryName)
+    return render_template('categoryItems.html', items=items)
+
+
 @app.route('/catalog/<string:categoryName>/<string:itemName>')
 def itemPage(categoryName, itemName):
     description = str(session.query(Item.description).filter_by(category=categoryName, name=itemName).one())[3:-3]
-    return description
+    return render_template('item.html', description=description)
+
+
 @app.route('/catalog/add', methods=['GET', 'POST'])
 def addPage():
-    if(request.method == 'POST'):
-        newItem = Item(name=request.form['name'], category=request.form['category'], description=request.form['description'])
+    if (request.method == 'POST'):
+        newItem = Item(name=request.form['name'], category=request.form['category'],
+                       description=request.form['description'])
         session.add(newItem)
         session.commit()
+        return True
     else:
-        return "page to create new Item"
-@app.route('/catalog/<string:itemName>/edit')
+        return render_template('addItem.html')
+
+
+@app.route('/catalog/<string:itemName>/edit', methods=['GET', 'POST'])
 def editPage(itemName):
-    if(request.method == 'POST'):
-        session.query(itemName).update({"name":request.form['name'], "category":request.form['category'], "description":request.form['description']})
+    if (request.method == 'POST'):
+        item = session.query(Item).filter_by(name=itemName).one()
+        item.name = request.form['name']
+        item.category = request.form['category']
+        item.description = request.form['description']
+        session.add(item)
         session.commit()
+        return redirect('/', code=302)
     else:
-        return "page to edit item"
-@app.route('/catalog/<string:itemName>/delete')
+        return render_template('editItem.html')
+
+
+@app.route('/catalog/<string:itemName>/delete', methods=['GET', 'POST'])
 def deletePage(itemName):
-    if(request.method == 'POST'):
-        session.query(Item).delete({"name":request.form['name']})
+    if (request.method == 'POST'):
+        item = session.query(Item).filter_by(name=itemName).one()
+        session.delete(item)
         session.commit()
+        return redirect('/', code=302)
     else:
-        return "page to delete item"
+        return render_template('deleteItem.html')
+
+
 @app.route('/catalog.json')
 def json():
     items = session.query(Item).all()
     return jsonify(items=[i.serialize for i in items])
 
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 8000)
+    app.run(host='0.0.0.0', port=8000)
